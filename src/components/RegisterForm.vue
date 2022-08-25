2fdd1407c431731dc94bca5c1ae7c505d77c8daf
Sabelo Mdashe <selbysabelomdashe@gmail.com>
2022-08-25 16:06:30 +0200

    frontend

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -11,17 +11,17 @@
           <input type="text" id="last_name" v-model="last_name" class="p-1" />
         </div>
       </div>
-      <label class="mt-5">Email:</label>
+      <label class="mt-5 email">Email:</label>
       <input
         type="email"
         id="user_email"
-        v-model="user_emial"
+        v-model="user_email"
         class="p-1"
         required
       />
-      <div class="d-flex gap-5">
+      <div class="d-flex gap-5 passwords">
         <div class="inputs d-flex">
-          <label class="mt-5">Password:</label>
+          <label class="mt-5 passwords">Password:</label>
           <input
             type="password"
             id="user_password"
@@ -31,7 +31,7 @@
           />
         </div>
         <div class="inputs d-flex">
-          <label class="mt-5">Confirm Password:</label>
+          <label class="mt-5 confirm">Confirm Password:</label>
           <input
             type="password"
             id="user_password"
@@ -58,5 +58,27 @@ form,
 
 input {
   width: 250px;
+}
+
+/* MOBILE RESPONSIVENESS
+MEDIA QUERIES*/
+
+@media screen and (max-width: 415px) {
+  .gap-5 {
+    flex-direction: column;
+    gap: 10px !important;
+  }
+
+  .passwords {
+    margin-top: 0.5rem !important;
+  }
+
+  .email {
+    margin-top: 1rem !important;
+  }
+
+  .confirm {
+    margin-top: 0rem !important;
+  }
 }
 </style>
