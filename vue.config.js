32f9346ad3c2c3bd9dafc1b7c343139493a18ced
Sabelo Mdashe <selbysabelomdashe@gmail.com>
2022-09-09 10:42:13 +0200

    modified the config file

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    // Your API Link
-    proxy: "http://localhost:2121/",
-  },
-});
+// const { defineConfig } = require("@vue/cli-service");
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer: {
+//     // Your API Link
+//     proxy: "http://localhost:2121/",
+//   },
+// });
